3601d98d1fed59b711ccb512e9852cb01000e69c
Trey Huffine <treyhuffine@gmail.com>
2015-06-26 18:45:54 -0700

    Add login to factory and implement logout

--- main.js
+++ main.js
@@ -22,14 +22,58 @@ app
       redirectTo: "/"
     });
 })
-.controller("MainCtrl", function($scope, $rootScope) {
+.factory("Auth", function($rootScope, $location) {
+  function Auth(){
+  }
+
+  Auth.register = function(user) {
+
+  };
+  Auth.login = function(user) {
+    $rootScope.afAuth.$authWithPassword({
+      email: user.email,
+      password: user.password
+    })
+    .then(function(authData) {
+      console.log(authData);
+      // Use authData.uid to get current user information
+    })
+    .catch(function(error) {
+      console.log(error)
+    });
+  };
+  Auth.monitorAuth = function() {
+    $rootScope.afAuth.$onAuth(function(authData) {
+      console.log(authData);
+      if (authData) {
+        $rootScope.authData = authData;
+        console.log($rootScope.fbRef.child(authData.uid));
+        $rootScope.fbRef.child(authData.uid).once("value", function(snapshot) {
+          console.log(snapshot.val());
+          $rootScope.currentUser = snapshot.val();
+        });
+        console.log($rootScope.currentUser);
+        $location.path("/welcome");
+      }
+    });
+  };
+  Auth.logout = function() {
+    $rootScope.afAuth.$unauth();
+  };
+  return Auth;
+})
+.controller("MainCtrl", function($scope, $rootScope, $location, Auth) {
   var auth = $rootScope.afAuth;
+  if ($rootScope.authData) {
+    $location.path("/welcome");
+  }
   $scope.registerUser = function() {
     auth.$createUser({
       email: $scope.newUser.email,
       password: $scope.newUser.password
     })
     .then(function(data) {
+      console.log($scope.newUser);
       $rootScope.fbRef.child("users").child(data.uid).set({
         fullName: $scope.newUser.fullName,
         username: $scope.newUser.username,
@@ -39,19 +83,19 @@ app
     .catch(function(error) {
       console.log(error);
     });
-    $scope.newUser = {};
+    auth.$onAuth(function(authData) {
+      $rootScope.authData = authData;
+      $rootScope.fbRef.once("value", function(snapshot) {
+        console.log(snapshot.val());
+      });
+      $location.path("/welcome");
+    });
   };
   $scope.loginUser = function() {
-    auth.$authWithPassword({
-      email: $scope.currentUser.email,
-      password: $scope.currentUser.password
-    })
-    .then(function(authData) {
-      console.log(authData);
-    })
-    .catch(function(error) {
-      console.log(error)
-    });
-    $scope.currentUser = {};
-  }
+    Auth.login($scope.currentUser);
+  };
+  Auth.monitorAuth();
+})
+.controller("WelcomeCtrl", function($scope, Auth) {
+  $scope.logout = Auth.logout;
 });
