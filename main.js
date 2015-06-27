b8ac802935d7042c3a528fdeba78fca48bdccd19
Trey Huffine <treyhuffine@gmail.com>
2015-06-26 17:27:42 -0700

    Implement firebase authentication

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 'use strict';
 
-var app = angular.module("lyve", ["firebase", "ngRoute"]);
+var app = angular.module("lyve", ["ngRoute", "firebase"]);
 
 app
+.constant("fbUrl", "https://lyve.firebaseio.com/")
+.run(function($rootScope, $firebaseAuth, $window, fbUrl) {
+  $rootScope.fbRef = new $window.Firebase(fbUrl);
+  $rootScope.afAuth = $firebaseAuth($rootScope.fbRef);
+})
 .config(function($routeProvider) {
   $routeProvider
     .when("/", {
@@ -17,5 +22,36 @@ app
       redirectTo: "/"
     });
 })
-.controller("MainCtrl", function($scope) {
+.controller("MainCtrl", function($scope, $rootScope) {
+  var auth = $rootScope.afAuth;
+  $scope.registerUser = function() {
+    auth.$createUser({
+      email: $scope.newUser.email,
+      password: $scope.newUser.password
+    })
+    .then(function(data) {
+      $rootScope.fbRef.child("users").child(data.uid).set({
+        fullName: $scope.newUser.fullName,
+        username: $scope.newUser.username,
+        email: $scope.newUser.email
+      });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+    $scope.newUser = {};
+  };
+  $scope.loginUser = function() {
+    auth.$authWithPassword({
+      email: $scope.currentUser.email,
+      password: $scope.currentUser.password
+    })
+    .then(function(authData) {
+      console.log(authData);
+    })
+    .catch(function(error) {
+      console.log(error)
+    });
+    $scope.currentUser = {};
+  }
 });
