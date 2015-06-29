7f52664ea73f8f590f827850576adce1fa6895eb
Trey Huffine <treyhuffine@gmail.com>
2015-06-28 19:04:03 -0700

    Add followers/following to profile

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -11,5 +11,19 @@ app.controller("ProfileCtrl", function($rootScope, $scope, $timeout, UserAuth, $
   $scope.isPostMaxed = function() {
     return $scope.remainingCharacters() < 0;
   };
+  $scope.getFollowersCount = function() {
+    if ($rootScope.currentUser && $rootScope.currentUser.followers) {
+      return $rootScope.currentUser.followers.length;
+    } else {
+      return 0;
+    }
+  };
+  $scope.getFollowingCount = function() {
+    if ($rootScope.currentUser && $rootScope.currentUser.following) {
+      return $rootScope.currentUser.following.length;
+    } else {
+      return 0;
+    }
+  };
   $scope.currentUserPosts = Post.showCurrentUserPosts();
 });
